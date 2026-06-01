65feca6199565baad255a5ef0c0217ecf68b08c9
lianet <jack.tolmak@yandex.ru>
2026-06-01 22:22:28 +0300

    feat: add choose block adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -221,10 +221,12 @@ function initHomeSwipers() {
 		new Swiper(chooseEl, {
 			slidesPerView: 1,
 			spaceBetween: 10,
+			autoHeight: true,
 			breakpoints: {
 				991: {
 					slidesPerView: 2,
 					spaceBetween: 20,
+					autoHeight: false,
 				},
 			},
 			navigation: {
