60535551da4a7f98f65936ff711254b2211471ff
lianet <jack.tolmak@yandex.ru>
2026-06-06 18:46:10 +0300

    fix: advantages block

--- template-parts/home/advantages.php
+++ template-parts/home/advantages.php
@@ -5,11 +5,35 @@
  * @package alergobot
  */
 
-$faq_items = alergobot_home_rows('items');
-$faq_col1  = array_slice($faq_items, 0, 4);
-$faq_col2  = array_slice($faq_items, 4);
+$faq_items = [];
 
-$render_faq_item = static function (array $item) {
+if (function_exists('have_rows') && have_rows('items')) {
+	while (have_rows('items')) {
+		the_row();
+		$faq_items[] = [
+			'question' => get_sub_field('question'),
+			'answer'   => get_sub_field('answer'),
+			'is_open'  => (bool) get_sub_field('is_open'),
+		];
+	}
+}
+
+$faq_col1 = [];
+$faq_col2 = [];
+
+foreach ($faq_items as $index => $item) {
+	if (($index + 1) % 2 === 0) {
+		$faq_col2[] = $item;
+	} else {
+		$faq_col1[] = $item;
+	}
+}
+
+$render_faq_item = static function ($item) {
+	if (!is_array($item)) {
+		return;
+	}
+
 	$is_open = !empty($item['is_open']);
 	?>
 	<div class="accordion__item<?php echo $is_open ? ' _active' : ''; ?>" data-animate="bottom">
@@ -21,7 +45,7 @@ $render_faq_item = static function (array $item) {
 		</button>
 		<div class="accordion__body">
 			<div class="accordion__inner">
-				<p class="accordion__answer"><?php echo esc_html($item['answer'] ?? ''); ?></p>
+				<p class="accordion__answer"><?php echo nl2br(esc_html($item['answer'] ?? '')); ?></p>
 			</div>
 		</div>
 	</div>
@@ -35,7 +59,7 @@ $render_faq_item = static function (array $item) {
 				<span class="faq__tag tag" data-animate="scale"><?php echo esc_html($tag); ?></span>
 			<?php endif; ?>
 			<?php if ($title = alergobot_home_get('title')) : ?>
-				<h2 class="faq__title title-md" data-animate="bottom"><?php echo esc_html($title); ?></h2>
+				<h2 class="faq__title title-md" data-animate="bottom"><?php echo nl2br(esc_html($title)); ?></h2>
 			<?php endif; ?>
 		</div>
 		<div class="faq__columns accordion" data-accordion="">
