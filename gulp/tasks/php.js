eab30870e01e15be328d8afd5685ea74fd27a969
lianet <jack.tolmak@yandex.ru>
2026-06-06 13:50:08 +0300

    add wordpress start theme

--- gulp/tasks/php.js
+++
@@ -1,9 +0,0 @@
-export const php = () => {
-	if (!app.path.src.php) {
-		return app.gulp.dest('.', { cwd: process.cwd() });
-	}
-	return app.gulp
-		.src(app.path.src.php, { base: 'src/theme-php' })
-		.pipe(app.gulp.dest(app.path.build.php))
-		.pipe(app.plugins.browsersync.stream());
-};
