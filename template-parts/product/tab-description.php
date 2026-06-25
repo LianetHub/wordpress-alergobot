021b2492089c795c7bfd158f1ed6ff1197ec96ce
Lianet <Jack-Tolmak@ya.ru>
2026-06-25 14:50:40 +0300

    fix: empty col in table

--- template-parts/product/tab-description.php
+++ template-parts/product/tab-description.php
@@ -10,6 +10,14 @@ $post_id = get_the_ID();
 $description = (string) alergobot_get_post_field( 'product_panel_description', $post_id );
 $table_rows  = alergobot_get_post_field( 'product_panel_table', $post_id ) ?: array();
 
+$show_allergen_en = false;
+foreach ( $table_rows as $row ) {
+	if ( '' !== trim( (string) ( $row['name_en'] ?? '' ) ) ) {
+		$show_allergen_en = true;
+		break;
+	}
+}
+
 if ( ! $description && ! $table_rows ) {
 	return;
 }
@@ -31,14 +39,18 @@ $is_active = (bool) get_query_var( 'product_tab_active' );
 						<colgroup>
 							<col class="product-table__col-num">
 							<col class="product-table__col-name">
-							<col class="product-table__col-allergen">
+							<?php if ( $show_allergen_en ) : ?>
+								<col class="product-table__col-allergen">
+							<?php endif; ?>
 							<col class="product-table__col-code">
 						</colgroup>
 						<thead>
 							<tr>
 								<th scope="col">№</th>
 								<th scope="col"><?php esc_html_e( 'Аллерген', 'alergobot' ); ?></th>
-								<th scope="col">Allergen</th>
+								<?php if ( $show_allergen_en ) : ?>
+									<th scope="col">Allergen</th>
+								<?php endif; ?>
 								<th scope="col"><?php esc_html_e( 'Код', 'alergobot' ); ?></th>
 							</tr>
 						</thead>
@@ -47,7 +59,9 @@ $is_active = (bool) get_query_var( 'product_tab_active' );
 								<tr>
 									<td><?php echo esc_html( (string) ( $row_index + 1 ) ); ?></td>
 									<td><?php echo esc_html( $row['name_ru'] ?? '' ); ?></td>
-									<td><?php echo esc_html( $row['name_en'] ?? '' ); ?></td>
+									<?php if ( $show_allergen_en ) : ?>
+										<td><?php echo esc_html( $row['name_en'] ?? '' ); ?></td>
+									<?php endif; ?>
 									<td><?php echo alergobot_breakable_code( $row['code'] ?? '' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></td>
 								</tr>
 							<?php endforeach; ?>
@@ -58,7 +72,9 @@ $is_active = (bool) get_query_var( 'product_tab_active' );
 								<td>
 									<button class="product-table__more" type="button" data-product-table-more="" aria-expanded="false"><?php esc_html_e( 'ЕЩЕ', 'alergobot' ); ?></button>
 								</td>
-								<td></td>
+								<?php if ( $show_allergen_en ) : ?>
+									<td></td>
+								<?php endif; ?>
 								<td></td>
 							</tr>
 						</tfoot>
